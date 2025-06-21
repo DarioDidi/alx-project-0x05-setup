2a0763074351950389d2d61d14bb6b030c45ad46
Dario Didi <dididario.dd2@gmail.com>
2025-06-21 16:06:31 +0300

    feat:handle api connection to image generator

--- alx-project-0x08/pages/index.tsx
+++ alx-project-0x08/pages/index.tsx
@@ -11,7 +11,23 @@ const Home: React.FC = () => {
 
   const handleGenerateImage = async () => {
     console.log("Generating Images");
-    console.log(process.env.NEXT_PUBLIC_GPT_API_KEY);
+    setIsLoading(true);
+    const resp = await fetch('/api/generate-image', {
+      method: "POST",
+      body: JSON.stringify({ prompt }),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+
+    if (!resp.ok) {
+      setIsLoading(false);
+      return;
+    }
+
+    const data = await resp.json();
+    setIsLoading(false);
+
   };
 
   return (
@@ -25,6 +41,8 @@ const Home: React.FC = () => {
         <div className="w-full max-w-md">
           <input
             type="text"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
             placeholder="Enter your prompt here..."
             className="w-full p-3 border border-gray-300 rounded-lg mb-4"
           />
@@ -32,10 +50,9 @@ const Home: React.FC = () => {
             onClick={handleGenerateImage}
             className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200"
           >
-            {/*{
-              isLoading? "Loading..." : "Generate Image"
-            }*/}
-            Generate Image
+            {
+              isLoading ? "Loading..." : "Generate Image"
+            }
           </button>
         </div>
         {imageUrl && <ImageCard action={() => setImageUrl(imageUrl)} imageUrl={imageUrl} prompt={prompt} />}
